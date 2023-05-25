94fc33be999b6582b163919223538b2b2863b666
Julio Cesar Chacon Vilela <julio.chacon@neoris.com>
2023-05-25 17:47:56 -0500

    fin seccion 15 - proteccion de rutas

--- 07-heroes-app-spa/src/router/AppRouter.jsx
+++ 07-heroes-app-spa/src/router/AppRouter.jsx
@@ -3,6 +3,8 @@ import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
 //import { Navbar } from "../ui"
 import { Route, Routes } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
@@ -14,8 +16,22 @@ export const AppRouter = () => {
             <Route path="/dc" element={ <DcPage /> } />
             <Route path="/" element={ <Navigate to="/marvel" /> } /> */}
             
-            <Route path="/login" element={ <LoginPage /> } />
-            <Route path="/*" element={ <HeroesRoutes /> } />
+            <Route path="/login" element={
+                <PublicRoute>
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+
+            <Route path="/*" element={
+                <PrivateRoute>
+                  <HeroesRoutes />
+                </PrivateRoute>
+              }
+            />
+
+            {/* <Route path="/login" element={ <LoginPage /> } /> */}
+            {/* <Route path="/*" element={ <HeroesRoutes /> } /> */}
 
             
         </Routes>
--- 07-heroes-app-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-app-spa/src/ui/components/Navbar.jsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context';
 
 export const Navbar = () => {
 
     const navigate = useNavigate();
+    const { user, logout } = useContext(AuthContext);
+    //console.log(user);
 
     const onLogout = () => {
+        logout();
         navigate('/login', {
             replace: true
-        });
+        });        
     }
-
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -50,7 +54,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Julio
+                        { user?.name }
                     </span>
                     <button 
                         className='nav-item nav-link btn'
