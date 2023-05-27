da26c31e677637cd4f086099e3380d586307238b
Julio Cesar Chacon Vilela <julio.chacon@neoris.com>
2023-05-26 22:01:00 -0500

    avances seccion 18 - redux

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getPokemons } from "./store/slices/pokemon";
+
+
+export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        dispatch( getPokemons() );
+
+    }, [])
+
+
+
+    return (
+        <>
+            <h1>PokemonApp</h1>
+            <hr />
+
+            <ul>
+
+            </ul>
+        </>
+    )
+}
