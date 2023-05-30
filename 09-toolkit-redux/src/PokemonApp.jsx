1b360486630a80e2b2ea0946613459e4638ff04c
Julio Cesar Chacon Vilela <julio.chacon@neoris.com>
2023-05-29 19:09:56 -0500

    fin seccion 18 - react redux

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getPokemons } from "./store/slices/pokemon";
 
 
 export const PokemonApp = () => {
 
     const dispatch = useDispatch();
+
+    const { isLoading, pokemons, page } = useSelector( state => state.pokemons );
 
     useEffect(() => {
 
@@ -19,10 +21,22 @@ export const PokemonApp = () => {
         <>
             <h1>PokemonApp</h1>
             <hr />
+            <span>Loading: { isLoading ? 'True' : 'False' }</span>
 
             <ul>
-
+                {
+                    pokemons.map(pokemon =>
+                        <li key={ pokemon.name }>{ pokemon.name }</li>
+                    )
+                }
             </ul>
+
+            <button
+                disabled={ isLoading }
+                onClick={ () => dispatch( getPokemons(page) ) }
+            >
+                Next
+            </button>
         </>
     )
 }
--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -6,10 +6,12 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { PokemonApp } from './PokemonApp.jsx'
+import { TodoApp } from './TodoApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     {/* <App /> */}
-    <PokemonApp />
+    {/* <PokemonApp /> */}
+    <TodoApp />
   </Provider>,
 )
